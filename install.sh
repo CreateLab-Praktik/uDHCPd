7f95ceea10751469dc5e7e305128badd33366ce2
Michal <mikh3x4@gmail.com>
2018-12-05 04:04:31 +0000

    added symlink install

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env sh
 
+FOLDER=$(dirname $(realpath "$0"))
+cd $FOLDER
 
-
-sudo apt install udhcpd
+sudo apt install -y udhcpd
 
 sudo systemd stop udhcpd
 sudo systemd disable udhcpd
+
+sudo ln -sf $FOLDER/udhcpd.conf /etc/udhcpd.conf
+sudo ln -sf $FOLDER/udhcpd /etc/default/udhcpd
+
+
