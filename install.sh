1b10000b0bee86597984233c9870276bbbb88a72
Michal <mikadam@stanford.edu>
2018-12-10 10:13:20 +0000

    moved to our own service file as it works correctly on boot

--- install.sh
+++ install.sh
@@ -11,4 +11,10 @@ dpkg -s udhcpd || { \
 sudo ln -sfn $FOLDER/udhcpd.conf /etc/udhcpd.conf
 sudo ln -sfn $FOLDER/udhcpd /etc/default/udhcpd
 
+for file in *.service; do
+    [ -f "$file" ] || break
+    sudo ln -s $FOLDER/$file /lib/systemd/system/
+done
+
+sudo systemctl daemon-reload
 
